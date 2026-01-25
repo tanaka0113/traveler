dd7261576482b46d69787b44530f30eae69b15e5
tanaka0113 <tanahide0113@gmail.com>
2026-01-25 10:19:17 +0900

    マイページのビューと背景画像を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  # devise の設定（これはそのまま）
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # ★ここを追加する
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :users, only: [:index, :show]
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   root "homes#top"
